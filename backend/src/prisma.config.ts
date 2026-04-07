f959106df55cfde4468f62fd59a56caf0e25cd24
Zion Musa <musazion986@gmail.com>
2026-04-07 12:44:37 +0100

    fix: migration commmand for vercel deployment

--- backend/src/prisma.config.ts
+++ backend/src/prisma.config.ts
@@ -8,9 +8,9 @@ import { defineConfig } from "prisma/config";
 dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "src/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "src/prisma/migrations",
   },
   datasource: {
     url: process.env.DATABASE_URL,
